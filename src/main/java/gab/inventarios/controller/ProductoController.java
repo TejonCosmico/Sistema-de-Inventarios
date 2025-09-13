6c5b18fe7c2f30078efd5678c3eb25141e96adba
TejonCosmico <127999859+TejonCosmico@users.noreply.github.com>
2025-09-13 14:09:12 -0300

    buscarProductoPorId agregado

--- src/main/java/gab/inventarios/controller/ProductoController.java
+++ src/main/java/gab/inventarios/controller/ProductoController.java
@@ -1,10 +1,12 @@
 package gab.inventarios.controller;
 
+import gab.inventarios.exception.ResourceNotFoundException;
 import gab.inventarios.models.Producto;
 import gab.inventarios.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +32,15 @@ public class ProductoController {
     public Producto agregarProducto(@RequestBody Producto producto) {
         logger.info("Producto a agregar: " + producto);
         return this.productoService.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable int id) {
+        Producto producto = this.productoService.buscarProductoPorId(id);
+        if (producto != null) {
+            return ResponseEntity.ok(producto);
+        } else {
+            throw new ResourceNotFoundException("No se encontro el producto con id " + id);
+        }
     }
 }
